326826bed372b4d07be8007836e2b4da95cdb23d
Reilley <reilleygray@gmail.com>
2025-12-21 09:19:32 +1100

    Add bitwarden container

---
+++ bitwarden.tf
@@ -0,0 +1,22 @@
+module "bitwarden" {
+  source = "./modules/service"
+
+  name    = "bitwarden"
+  image   = "vaultwarden/server:latest"
+  public  = true
+  port    = 80
+  network = [docker_network.traefik.id]
+
+  env = [
+    "TZ=Australia/Melbourne",
+    "PGID=1000",
+    "PUID=1000",
+  ]
+
+  volumes = [
+    {
+      container_path = "/data"
+      host_path      = "/home/reilley/appdata/bitwarden"
+    },
+  ]
+}
