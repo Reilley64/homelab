698b063e474e62c83e2c482f6143c1f2dddca65f
Reilley <reilleygray@gmail.com>
2025-12-20 20:32:58 +1100

    Fix dynamic traefik label

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -47,11 +47,12 @@ resource "docker_container" "container" {
     value = "false"
   }
 
-  labels {
-    count = var.public ? 1 : 0
-
-    label = "traefik.http.routers.${var.name}.rule"
-    value = "Host('${var.name}.example.invalid')"
+  dynamic "labels" {
+    for_each = var.public ? [1] : []
+    content {
+      label = "traefik.http.routers.${var.name}.rule"
+      value = "Host('${var.name}.example.invalid')"
+    }
   }
 
   network_mode = var.forward
