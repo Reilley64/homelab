5f0e7671f8131cffd1ce74ef631afe114eec0c5f
Reilley <reilleygray@gmail.com>
2025-12-22 09:28:22 +1100

    Add duin

--- main.tf
+++ main.tf
@@ -12,3 +12,32 @@ resource "docker_image" "alpine" {
   name         = "alpine:latest"
   keep_locally = false
 }
+
+module "diun" {
+  source = "./modules/service"
+
+  name  = "diun"
+  image = "crazymax/diun:v4.30.0"
+
+  env = concat(local.shared_env, [
+    "DIUN_WATCH_WORKERS=20",
+    "DIUN_WATCH_SCHEDULE=0 */6 * * *",
+    "DIUN_WATCH_JITTER=30s",
+    "DIUN_PROVIDERS_DOCKER=true",
+  ])
+
+  command = [
+    "serve",
+  ]
+
+  volumes = [
+    {
+      container_path = "/var/run/docker.sock"
+      host_path      = "/var/run/docker.sock"
+    },
+    {
+      container_path = "/data"
+      host_path      = "/home/reilley/appdata/diun"
+    },
+  ]
+}
