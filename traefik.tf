c7a114e131103fc8834a8620a52c44f7ec7d7d8e
Reilley <reilleygray@gmail.com>
2026-01-01 12:54:30 +1100

    Bump Traefik and Vaultwarden image versions

--- traefik.tf
+++ traefik.tf
@@ -7,7 +7,7 @@ module "traefik" {
   source = "./modules/service"
 
   name     = "traefik"
-  image    = "traefik:v3.6.5"
+  image    = "traefik:v3.6.6"
   networks = [docker_network.traefik.id]
 
   env = local.shared_env
